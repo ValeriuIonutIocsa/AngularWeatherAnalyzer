5853a8b574816c0f75b9c458b34d480cb2d4c354
Iocsa, Valeriu-Ionut <valeriu.iocsa@vitesco.com>
2024-03-25 01:14:18 +0200

    changed to openmeteo api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,19 +1,6 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
   production: false,
   supabaseUrl: 'https://fbdyneieueiconwfsvrl.supabase.co',
   supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZiZHluZWlldWVpY29ud2ZzdnJs' + 
   'Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzg2MjM1MzAsImV4cCI6MTk5NDE5OTUzMH0.CmCGmCoHE_mtDKrTfMDjfedOcU5d44d8GFa0Wez18ek'
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
